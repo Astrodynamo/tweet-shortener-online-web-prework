2c2618dd095273220d82a42d3e1a080ad5a5b066
Amanda King <amapickell@gmail.com>
2019-05-29 23:10:45 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,7 +31,9 @@ def bulk_tweet_shortener (tweets)
 end
 
 def selective_tweet_shortener (tweet)
-  if tweet.length > 140
-
+  if tweet > 140
+    word_substituter(tweet)
+  else
+    tweet
   end
 end
